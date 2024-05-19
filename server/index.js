34989e3876a1b8cd888fa71070c3f9bc03c91257
DEBANJAN MUKHERJEE <debanjanmukherjee015@gmail.com>
2024-05-19 22:14:41 +0530

    some fix

--- server/index.js
+++ server/index.js
@@ -2,21 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { userRouter } from "./controller/AuthController.js";
 import { ProfileRouter } from "./controller/ProfileController.js";
 import { initiativeRouter } from "./controller/Intiativerouter.js";
 import { ecoActionRouter } from "./controller/EcoActionController.js";
+
 dotenv.config();
+
 const app = express();
 
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    method: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials : true
-  })  
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
 );
-
+app.use(express.json());
 
 const db = mongoose.connect(process.env.MONGO_URI);
 
@@ -27,14 +30,21 @@ try {
 } catch (error) {
   console.log(error);
 }
-app.use(express.json());
+
 app.use("/auth", userRouter);
 app.use("/profile", ProfileRouter);
 app.use("/initiative", initiativeRouter);
 app.use("/ecoAction", ecoActionRouter);
 
+// Serve static files from the React app's build directory
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'build')));
 
+// Catch-all route to serve the index.html file for any other requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.listen(3000, (req, res) => {
+app.listen(3000, () => {
   console.log("server at 3000");
 });
